55e5d083a5d72cefa96d60513db6a644fe9b308f
LucasAmaralPires <lucasampires@gmail.com>
2023-09-12 16:15:12 -0300

    Add files via upload

--- reddit.js
+++ reddit.js
@@ -29,30 +29,8 @@ function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 }   
 
-var js;
+var js = {};
 
-/*
-async function select_chain()
-{
-    while (true) {
-        else if (num_answer === "2")
-        {
-        }
-        else if (num_answer === "3") //Like
-        {
-//            Main.main(["freechains", "chain", chains[num_chains], "like", hash[i]], (ans) => {block = ans;});
-                        
-            return;
-        }
-        else if (num_answer === "4") //Dislike
-        {
-//            Main.main(["freechains", "chain", chains[num_chains], "dislike", hash[i]], (ans) => {block = ans;});
-
-            return;
-        }
-    }
-}
-*/
 async function show(arg)
 {
     let block;
@@ -61,7 +39,7 @@ async function show(arg)
     let json_block;
     let j;
     i = 1;
-    Main.main(["freechains", "chain",  `#${js.username[js.current_select].select_subreddit}`, "consensus", `--port=${js.username[js.current_select].port}`], (ans) => {hash = ans;});
+    Main.main(["freechains", "chain",  `#${js.select_subreddit}`, "consensus", `--port=${js.port}`], (ans) => {hash = ans;});
     await delay(1000);
     hash = hash.slice(0,-1);
     hash = hash.split(' ');
@@ -69,7 +47,7 @@ async function show(arg)
     {
         for(i = 1; i < hash.length; i++)
         {
-            Main.main(["freechains", "chain",  `#${js.username[js.current_select].select_subreddit}`, "get", "payload", hash[i], `--port=${js.username[js.current_select].port}`], (ans) => {payload = ans;});
+            Main.main(["freechains", "chain",  `#${js.select_subreddit}`, "get", "payload", hash[i], `--port=${js.port}`], (ans) => {payload = ans;});
             await delay(1000);
             payload = payload.split('\n');
             payload.splice(0,1);
@@ -114,64 +92,56 @@ async function like(arg)
     console.log(arg);
 }
 
-async function start(arg)
+async function start(arg, pass)
 {
     let i;
 
-    js = {
-        "user": arg[1],
-        "password": [
-          "41783E6207C61E0ED6499C7EE800897E2930610623C3EDFAA8EE8F6ADE1593B5",
-          "DAF8C7C6A5B1127624007983F1A3235042F7382E9E1D859D9949E19A99DA528341783E6207C61E0ED6499C7EE800897E2930610623C3EDFAA8EE8F6ADE1593B5"
-        ],
-        "port": "8330",
-        "subreddits": [],
-        "select_subreddit": ""
-      };
+    js;
 
-    for(i = 0; i < js.username.length; i++)
+    js.user = arg[1];
+    js.select_subreddit = "";
+
+    if(arg[3] != undefined)
     {
-        if(arg[1] == js.username[i].user)
-        {
-            Main.main(["freechains", "keys", "pubpvt", arg[2], `--port=${js.username[i].port}`], (ans) => {
-                if(ans != (js.username[i].password[0] + " " + js.username[i].password[1] + "\n"))
-                {
-                    console.log("Incorrect password!");
-                    process.exit();
-                }
-            });
-            await delay(1000);
-
-            if(arg[3] != undefined)
-            {
-                js.username[i].port = arg[3];
-            }
-          
-            js.current_select = i;
-
-            console.log("Log in successful")
-
-            break;
-        }
+        js.port = arg[3];
     }
+    else
+    {
+        js.port = 8330;
+    }
+  
+    Main.main(["freechains", "keys", "pubpvt", arg[2], `--port=${js.port}`], (ans) => {
+        Main.main(["freechains", "chains", "list", `--port=${js.port}`], (ans_create) => {
+                ans = ans.slice(0,-1);
+                ans = ans.split(' ');
+                ans_create = ans_create.slice(0,-1);
+                ans_create = ans_create.split(' ');
+                js.subreddits = ans_create;
+                js.password = ans;
+            });
+        });
+    await delay(3000);
+
+    console.log("Log in successful")
+    
     fs.writeFileSync("config.json", JSON.stringify(js, null, 2));                
 }
 
 async function leave(arg)
 {
     let j;
-    for (j = 0; j < js.username[js.current_select].subreddits.length; j++)
+    for (j = 0; j < js.subreddits.length; j++)
     {
-        if (arg[1] == js.username[js.current_select].subreddits[j])
+        if (arg[1] == js.subreddits[j])
         {
-            Main.main(["freechains", "chains", "leave", `#${arg[1]}`, `--port=${js.username[js.current_select].port}`], () => {
+            Main.main(["freechains", "chains", "leave", `#${arg[1]}`, `--port=${js.port}`], () => {
                 console.log("Left subreddit");
             })
             await delay(2000);
-            js.username[js.current_select].subreddits.splice(j,j);
+            js.subreddits.splice(j,j);
             break;   
         }
-        else if(j == js.username[js.current_select].subreddits.length-1)
+        else if(j == js.subreddits.length-1)
         {
             console.log("Subreddit not found");
         }
@@ -181,13 +151,13 @@ async function leave(arg)
 
 async function post(arg)
 {
-    if(js.username[js.current_select].select_subreddit == "")
+    if(js.select_subreddit == "")
     {
         console.log("Select a subreddit first.");
     }
     else
     {
-        Main.main(["freechains", "chain", `#${js.username[js.current_select].select_subreddit}`, "post", "inline", arg[1], `--sign=${js.username[js.current_select].password[1]}`, `--port=${js.username[js.current_select].port}`], (ans) => {process.stdout.write(ans)});
+        Main.main(["freechains", "chain", `#${js.select_subreddit}`, "post", "inline", arg[1], `--sign=${js.password[1]}`, `--port=${js.port}`], (ans) => {process.stdout.write(ans)});
         await delay(2000);         
     }
 
@@ -216,11 +186,11 @@ async function select(arg)
 async function create_join(arg)
 {
 
-    Main.main(["freechains", "chains", "join", `#${arg[1]}`, js.username[js.current_select].password[0], `--port=${js.username[js.current_select].port}`], () => {
+    Main.main(["freechains", "chains", "join", `#${arg[1]}`, js.password[0], `--port=${js.port}`], () => {
            console.log("Subreddit created/joined!");
     })
     await delay(2000);
-    js.username[js.current_select].subreddits.push(arg[1]);
+    js.subreddits.push(arg[1]);
 
 }
 
@@ -236,37 +206,25 @@ async function main(arg)
     }
     else
     {
-        try
+/*        try
         {
+
             js = JSON.parse(fs.readFileSync("config.json"));
-//        console.log(js);
 
         }
-        catch(err)
+        catch (err)
         {
-            if(arg[0] == "login")
-            {
-                if(arg.length < 3 || arg.length > 4)
-                {
-                    console.log("Wrong number of arguments.");
-                }
-                else
-                {
-                    await start(arg);
-                }
-            }
-            else
-            {
-                console.log("You must first login before using reddit!");
-            }
-        }
-/*
-        if (js.login == false && arg[0] != "login")
+
+        }*/
+
+        if(js[0] != undefined && arg[0] != "login")
         {
             console.log("You must first login before using reddit!");
+            process.exit();
         }
         else
         {
+
             let s_like = "like"
             switch (arg[0])
             {
@@ -277,7 +235,7 @@ async function main(arg)
                     }
                     else
                     {
-                        await start(arg);
+                        await start(arg, js);
                     }
                     break;
                 case "logoff":
@@ -287,9 +245,9 @@ async function main(arg)
                     }
                     else
                     {
-                        js.username[js.current_select].select_subreddit = "";
-                        js.current_select = 0; 
-                        js.login = false;
+                        fs.unlinkSync("config.json");
+                     //   fs.writeFileSync("config.json", JSON.stringify(js.password, null, 2));                
+                        process.exit();
                     }
                     break;
                 case "join":
@@ -310,7 +268,7 @@ async function main(arg)
                     }
                     else
                     {
-                        js.username[js.current_select].subreddits.forEach(function(entry) {
+                        js.subreddits.forEach(function(entry) {
                             console.log(entry);
                         });
                     }
@@ -376,8 +334,6 @@ async function main(arg)
                     console.log("Command not recognized!");
             }
         }
-        fs.writeFileSync("config.json", JSON.stringify(js, null, 2));
-*/
     }
 }
 
